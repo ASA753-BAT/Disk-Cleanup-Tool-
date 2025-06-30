b6ebc48186e77a9de0b0b87346819b4b5ac9795d
ASA753 <cehennemcennet123@gmail.com>
2025-06-30 18:22:57 +0300

    Add files via upload

---
+++ diskformat.bat
@@ -0,0 +1,43 @@
+@echo off
+:menu
+cls
+echo ===============================
+echo     DISK TEMIZLEME ARACI
+echo ===============================
+echo.
+echo Takili Diskler:
+echo -------------------------------
+wmic diskdrive get index,model,size
+echo -------------------------------
+echo.
+set /p diskno=Islem yapilacak DISK numarasini girin (sadece rakam): 
+
+echo.
+echo UYARI: Bu islem DISK %diskno% uzerindeki tum verileri SILECEK!
+set /p onay=Devam etmek istiyor musun? (E/H): 
+if /i not "%onay%"=="E" (
+    echo Islemi iptal ettiniz.
+    pause
+    goto menu
+)
+
+echo.
+echo Islem baslatiliyor...
+echo LUTFEN BEKLEYIN...
+
+(
+echo select disk %diskno%
+echo clean
+echo create partition primary
+echo format fs=ntfs quick
+echo assign
+echo exit
+) | diskpart >nul
+
+echo.
+echo ===============================
+echo    ISLEM TAMAMLANDI ✅
+echo ===============================
+echo.
+pause
+goto menu
